13c6cfd6f08e1615085f0542e6f6c098687ac575
kimnv57 <kimnv57@gmail.com>
2015-11-21 07:31:28 +0700

    home

--- main/src/team/hidro/highschoolsupport/controllers/HomeController.java
+++ main/src/team/hidro/highschoolsupport/controllers/HomeController.java
@@ -2,12 +2,13 @@ package team.hidro.highschoolsupport.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/home")
+	@RequestMapping(value={"/home","/"}, method=RequestMethod.GET)
 	public ModelAndView home(){
 		return new ModelAndView("home");
 	}
